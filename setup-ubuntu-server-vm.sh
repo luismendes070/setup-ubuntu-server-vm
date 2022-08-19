4166e3b5db818bfbe094462043b3107a1eec53ac
Luis Mendes <machadoluismendes@gmail.com>
2022-08-19 12:35:21 -0300

    Receiving a GPG error when running apt-get update

--- setup-ubuntu-server-vm.sh
+++ setup-ubuntu-server-vm.sh
@@ -14,5 +14,6 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 apt-cache madison docker-ce
   
